2ead1928f98f07301fa4d4861bbd44514bf1b665
wiily <willyhuang18@gmail.com>
2022-01-25 23:49:06 -0800

    creating the employee object

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,2 +1,12 @@
 //import the Employee constructor 
 const Employee = require('../lib/Employee');
+
+//making the new employee object 
+describe("Employee", ()=>{
+    describe("Initialization", ()=>{
+        it("should create an object with name, id, email", ()=>{
+            const employee = new Employee('Sara', 10, 'Sara.pro@yahoo.com');
+            
+        })
+    })
+})
