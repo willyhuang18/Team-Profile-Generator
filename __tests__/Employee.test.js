68fb07924fa349116972e93529011a60c64097eb
wiily <willyhuang18@gmail.com>
2022-01-25 23:54:49 -0800

    trying some jest expect

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,4 +1,5 @@
 //import the Employee constructor 
+const { string } = require('yargs');
 const Employee = require('../lib/Employee');
 
 //making the new employee object 
@@ -6,7 +7,8 @@ describe("Employee", ()=>{
     describe("Initialization", ()=>{
         it("should create an object with name, id, email", ()=>{
             const employee = new Employee('Sara', 10, 'Sara.pro@yahoo.com');
-            
+            //use jest to expect the output to be any string
+            expect(employee.name).toEqual(String)
         })
     })
 })
