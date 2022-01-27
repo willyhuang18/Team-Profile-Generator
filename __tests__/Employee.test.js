fc925d37ad06ff3efcadd9d6d68ced3c59709d6d
wiily <willyhuang18@gmail.com>
2022-01-26 16:22:38 -0800

    using the jest expect.any(constructor) to allow any input

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -6,28 +6,29 @@ describe("Employee", ()=>{
     describe("Initialization", ()=>{
         it("should create an object with name, id, email", ()=>{
             const employee = new Employee('Sara', 10, 'Sara.pro@yahoo.com');
-            //use jest to expect the output to be any string
-            expect(employee.name).toEqual(String);
-            expect(employee.id).toEqual(Number);
-            expect(employee.email).toEqual(String);
+            //use jest to expect the output to be any string, 
+            //using the jest expect.any(constructor) to allow any input
+            expect(employee.name).toEqual(expect.any(String));
+            expect(employee.id).toEqual(expect.any(Number));
+            expect(employee.email).toEqual(expect.any(String));
         });
         it("should create an object to get employee's name", ()=>{
             const employee = new Employee('Sara', 10, 'Sara.pro@yahoo.com');
 
             //using the getName method in the employee constructor
-            expect(employee.getName()).toEqual(String);
+            expect(employee.getName()).toEqual(expect.any(String));
         })
         it("should create an object to get employee's ID", ()=>{
             const employee = new Employee('Sara', 10, 'Sara.pro@yahoo.com');
 
             //using the getID method in the employee constructor
-            expect(employee.getId()).toEqual(Number);
+            expect(employee.getId()).toEqual(expect.any(Number));
         })
         it("should create an object to get employee's email", ()=>{
             const employee = new Employee('Sara', 10, 'Sara.pro@yahoo.com');
 
             //using the getEmail method in the employee constructor
-            expect(employee.getEmail()).toEqual(String);
+            expect(employee.getEmail()).toEqual(expect.any(String));
         })
     })
 })
