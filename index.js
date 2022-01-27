de5101dcefca287aadc036a280e70229ccdb51ce
wiily <willyhuang18@gmail.com>
2022-01-26 22:00:01 -0800

    //import files  from other folders

---
+++ index.js
@@ -0,0 +1,5 @@
+//import files 
+const HTMLpage = require('./src/HTML.js');
+const manager = require('./lib/Manager.js');
+const engineer = require('./lib/Engineer.js');
+const intern = require('./lib/Intern.js');
