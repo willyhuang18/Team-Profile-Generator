8bb3d9d023c30e19b187ac4a4d37cb1404ea63a8
wiily <willyhuang18@gmail.com>
2022-01-26 16:36:04 -0800

    test passed

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -6,7 +6,7 @@ describe("Engineer", ()=>{
     describe("Initialization", ()=>{
         //since the employee had Id, and name
         it("should create an object with Github", ()=>{
-            const engineer = new Engineer('Sara', 10, 'Sara.pro@yahoo.com');
+            const engineer = new Engineer('Sara', 10, 'Sara.pro@yahoo.com','sarapro123');
             //use jest to expect the output to be any string, 
             //using the jest expect.any(constructor) to allow any input
             expect(engineer.github).toEqual(expect.any(String));
