faa68e5731b16e9c0d646a7d446b11f679836791
wiily <willyhuang18@gmail.com>
2022-01-26 16:27:46 -0800

    test engineer fail because the engineer.js error

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,15 @@
+//import the Employee constructor 
+const Engineer = require('../lib/Engineer');
+
+//making the new employee object 
+describe("Engineer", ()=>{
+    describe("Initialization", ()=>{
+        //since the employee had Id, and name
+        it("should create an object with Github", ()=>{
+            const engineer = new Engineer('Sara', 10, 'Sara.pro@yahoo.com');
+            //use jest to expect the output to be any string, 
+            //using the jest expect.any(constructor) to allow any input
+            expect(engineer.github).toEqual(expect.any(String));
+        });
+    })
+})
