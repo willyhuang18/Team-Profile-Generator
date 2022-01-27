5148cc7cf90789c01443784189b1c302670778a4
wiily <willyhuang18@gmail.com>
2022-01-26 22:01:37 -0800

    install package for user input and write file

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const HTMLpage = require('./src/HTML.js');
 const manager = require('./lib/Manager.js');
 const engineer = require('./lib/Engineer.js');
 const intern = require('./lib/Intern.js');
+
+//install package for user input and write file
+const fs = require('fs');
+const inquirer = require('inquirer');
