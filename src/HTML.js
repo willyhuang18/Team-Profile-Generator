dabc75221750e37d8f08670350b2fa33c419f3af
wiily <willyhuang18@gmail.com>
2022-01-26 20:29:36 -0800

    create the Engineer card

--- src/HTML.js
+++ src/HTML.js
@@ -1,6 +1,6 @@
 //creating the card to indicate what role are creating card for
 //creating the card container for html inside JS
-const generateManager = function (manager) {
+const gManager = function (manager) {
     return `
     <div class="col">
         <div class="card">
@@ -17,4 +17,20 @@ const generateManager = function (manager) {
     `;
 }
 
-//create the 
+//create the Engineer card
+const gEngineer = function (engineer) {
+    return `
+    <div class="col">
+        <div class="card">
+            <div class="card-header">
+                <h1>${engineer.name}</h1> 
+                <h2>Manager</h2>
+            </div>
+            <div class="card-body">
+                <p>ID:${engineer.id}</p>
+                <p>Email:${engineer.email}<a href="mailto:${engineer.email}"></a></p>
+                <p>Github:${engineer.github}<a href="http://github.com/${engineer.email}"></a></p>
+            </div>
+    </div>
+    `;
+}
