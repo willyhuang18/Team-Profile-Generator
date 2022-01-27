83ba894c6a963b2358481e74535b5266cbcae60d
wiily <willyhuang18@gmail.com>
2022-01-26 22:36:53 -0800

    create new object to contain the constructor

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 //import files 
 const HTMLpage = require('./src/HTML.js');
-const manager = require('./lib/Manager.js');
-const engineer = require('./lib/Engineer.js');
-const intern = require('./lib/Intern.js');
+const Manager = require('./lib/Manager.js');
+const Engineer = require('./lib/Engineer.js');
+const Intern = require('./lib/Intern.js');
 
 //install package for user input and write file
 const fs = require('fs');
 const inquirer = require('inquirer');
 
+//create inquirer prompt for manager
 const managerQA = ()=>{
         inquirer.prompt([{
             type: "input",
@@ -27,4 +28,11 @@ const managerQA = ()=>{
             message: "Enter manager's office Number: ",
         }
     ])
+    .then(managerAnswer =>{
+        // This creates a constant with the name 'name, id, email, officeNumber'
+          const {name, id, email, officeNumber} = managerAnswer;
+          //create new object to contain the constructor 
+          const manager = new Manager(name, id, email, officeNumber);
+          console.log(manager);
+    })
 }
